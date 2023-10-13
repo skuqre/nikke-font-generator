39bc7c1f1e17555f43f40201e8ff5981f1d6bd50
skuqre <74248935+skuqre@users.noreply.github.com>
2023-10-14 00:29:08 +0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://skuqre.github.io',
+    base: '/nikke-font-generator',
+});
